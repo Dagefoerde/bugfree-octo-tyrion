404476d0dff2f8e22767f5e57c77e1113e414792
Julia65 <juledithmar@kaffeeschluerfer.com>
2013-11-10 01:36:15 -0800

    Medium.java

---
+++ Exercise 2/Ex 2 - de.wwu.pi.mdsd05.library.ref/src/de/wwu/pi/mdsd05/library/ref/data/Book.java
@@ -0,0 +1,13 @@
+package de.wwu.pi.mdsd05.library.ref.data;
+
+import de.wwu.pi.mdsd05.framework.data.AbstractDataClass;
+
+public class Book extends AbstractDataClass {
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Exercise 2/Ex 2 - de.wwu.pi.mdsd05.library.ref/src/de/wwu/pi/mdsd05/library/ref/data/Medium.java
@@ -0,0 +1,22 @@
+package de.wwu.pi.mdsd05.library.ref.data;
+
+import de.wwu.pi.mdsd05.framework.data.AbstractDataClass;
+
+public abstract class Medium extends AbstractDataClass {
+	
+	protected String name;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
