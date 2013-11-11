6d7cbf1ad98ab822c6fa7391505e8138e69b2c07
the-Other-One <jan.dagefoerde@googlemail.com>
2013-11-11 08:09:12 +0100

    MediumService now lists all media.

--- Exercise 2/Ex 2 - de.wwu.pi.mdsd05.library.ref/src/de/wwu/pi/mdsd05/library/ref/logic/MediumService.java
+++ Exercise 2/Ex 2 - de.wwu.pi.mdsd05.library.ref/src/de/wwu/pi/mdsd05/library/ref/logic/MediumService.java
@@ -1,23 +1,48 @@
 package de.wwu.pi.mdsd05.library.ref.logic;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import de.wwu.pi.mdsd05.framework.logic.AbstractServiceProvider;
 import de.wwu.pi.mdsd05.framework.logic.ValidationException;
-import de.wwu.pi.mdsd05.library.ref.data.Loan;
+import de.wwu.pi.mdsd05.library.ref.data.Book;
+import de.wwu.pi.mdsd05.library.ref.data.CD;
 import de.wwu.pi.mdsd05.library.ref.data.Medium;
 
 public class MediumService extends AbstractServiceProvider<Medium> {
 	
+
+	CDService cdService;
+	BookService bookService;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7564384820420239045L;
+
 	//Constructor
 	protected MediumService() {
 		super();
 	}
 
-	public boolean validateLoan(String name) throws ValidationException {
+	public boolean validateMedium(String name) throws ValidationException {
 		if(name == null)
 			throw new ValidationException("name", "cannot be empty");
 		return true;
+	}
+	
+	public Collection<Medium> getAll() {
+		if (cdService == null) cdService = ServiceInitializer.getProvider().getCDService();
+		if (bookService == null) bookService = ServiceInitializer.getProvider().getBookService();;
+		
+		Collection<CD> cds = cdService.getAll();
+		Collection<Book> books = bookService.getAll();
+		
+		ArrayList<Medium> mediums = new ArrayList<Medium>(cds.size() + books.size());
+		mediums.addAll(cds);
+		mediums.addAll(books);
+		
+		return mediums;
 	}
 	
 }
