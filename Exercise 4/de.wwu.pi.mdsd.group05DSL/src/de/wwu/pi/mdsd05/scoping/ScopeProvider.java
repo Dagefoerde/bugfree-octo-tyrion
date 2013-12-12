a2f447a04cc7d27dab38fe572d8d65400874ee99
the-Other-One <jan.dagefoerde@googlemail.com>
2013-12-12 21:02:52 +0100

    Weeell, for me it works now. With Scoping!

--- Exercise 4/de.wwu.pi.mdsd.group05DSL/src/de/wwu/pi/mdsd05/scoping/ScopeProvider.java
+++ Exercise 4/de.wwu.pi.mdsd.group05DSL/src/de/wwu/pi/mdsd05/scoping/ScopeProvider.java
@@ -1,10 +1,14 @@
-package de.wwu.pi.mdsd.scoping;
+package de.wwu.pi.mdsd05.scoping;
 
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.Scopes;
 
-public class CRUDDslScopeProvider {
+import de.wwu.pi.mdsd05.group05DSL.EntryWindow;
+import de.wwu.pi.mdsd05.group05DSL.Field;
+import de.wwu.pi.mdsd05.group05DSL.Property;
+
+public class ScopeProvider {
 	/*public IScope scope_#DeclaringType#_#reference#
 	(#ContextType# ctx, EReference ref) {
 	Iterable<#ReferenceType#> elements = …;
@@ -13,9 +17,8 @@ public class CRUDDslScopeProvider {
 	
 	}*/
 	
-	public IScope scope_Field_property (Field ctx, EReference ref) {
-		Iterable<Field> elements = ctx.getCustomer().getAddresses();
+	public IScope scope_Field_property(Field ctx, EReference ref) {
+		Iterable<Property> elements = ((EntryWindow)ctx.eContainer()).getEntitytype().getProperties();
 		return Scopes.scopeFor(elements);
-	
 	}
 }
