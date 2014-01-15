f242cfd7fc207404a32d6e0549b08f827d70ec28
the-Other-One <jan.dagefoerde@googlemail.com>
2014-01-15 13:53:11 +0100

    modified abstractentrywindow

--- Exercise 5/de.wwu.pi.mdsd05.framework/src/de/wwu/pi/mdsd/framework/gui/AbstractEntryWindow.java
+++ Exercise 5/de.wwu.pi.mdsd05.framework/src/de/wwu/pi/mdsd/framework/gui/AbstractEntryWindow.java
@@ -2,7 +2,6 @@ package de.wwu.pi.mdsd.framework.gui;
 
 import java.awt.Container;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,7 +17,20 @@ public abstract class AbstractEntryWindow<E extends AbstractDataClass> extends A
 	
 	protected E currentEntity;
 	protected int curGridY = 0;
-	protected JButton btnSave;
+	//protected JButton btnSave;
+	protected ActionListener saveBtnActionListener = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			try {
+				if (saveAction())
+					AbstractEntryWindow.this.closeWindow();
+			} catch (NumberFormatException e) {
+				Util.showUserMessage("Wrong number Format", e.getMessage());
+			} catch (ParseException e) {
+				Util.showUserMessage("Wrong Date Format", "Was not able to parse the given Date '" + e.getMessage() + "'. Please format the date as follows: dd.mm.yyyy");
+			}
+		}
+	};
 	
 	public AbstractEntryWindow(AbstractWindow parent, E currentEntity) {
 		super(parent);
@@ -28,34 +40,23 @@ public abstract class AbstractEntryWindow<E extends AbstractDataClass> extends A
 	protected void createContents() {
 		Container panel = getPanel();
 
-		GridBagLayout gbl = new GridBagLayout();
-		gbl.columnWeights = new double[]{.1,.25, Double.MIN_VALUE};
-		panel.setLayout(gbl);
+		panel.setLayout(null);
 		
-		//reset Current Y-Value
-		curGridY = 0;
 		createFields();
 		
-		btnSave = new JButton(currentEntity.isNew() ? "Create" : "Update");
+		/*btnSave = new JButton(currentEntity.isNew() ? "Create" : "Update");
 		GridBagConstraints gbc_btnSave = new GridBagConstraints();
 		gbc_btnSave.insets = new Insets(0, 0, 5, 0);
 		gbc_btnSave.gridx = 1;
 		gbc_btnSave.gridy = getNextGridYValue();
 		panel.add(btnSave, gbc_btnSave);
-		btnSave.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				try {
-					if (saveAction())
-						AbstractEntryWindow.this.closeWindow();
-				} catch (NumberFormatException e) {
-					Util.showUserMessage("Wrong number Format", e.getMessage());
-				} catch (ParseException e) {
-					Util.showUserMessage("Wrong Date Format", "Was not able to parse the given Date '" + e.getMessage() + "'. Please format the date as follows: dd.mm.yyyy");
-				}
-			}
-		});
 		
+		
+		btnSave.addActionListener(saveBtnActionListener);*/
+		
+		/*
+		createLists() might be obsolete
+		 
 		JPanel fill1 = new JPanel();
 		GridBagConstraints gbc_fill1 = new GridBagConstraints();
 		gbc_fill1.gridx = 0;
@@ -72,7 +73,7 @@ public abstract class AbstractEntryWindow<E extends AbstractDataClass> extends A
 		gbc_separator.gridx = 0;
 		gbc_separator.gridy = getNextGridYValue();
 		gbc_separator.weighty = 0.5;
-		panel.add(separator, gbc_separator);
+		panel.add(separator, gbc_separator);*/
 	}
 	
 	@Override
